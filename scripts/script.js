e8f464d92430a476b7168f2c5bde5f488dc4de2e
Vinicius <vinicius.barretto9022@gmail.com>
2024-10-31 00:33:51 -0300

    add script to open edit popup and submit

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,31 @@
-const element = document.querySelector(".profile");
-const 
+let element = document.querySelector(".profile");
+let editBtnElement = element.querySelector(".button_edit");
+let popupElement = document.querySelector(".popup");
+let closeBtnElement = popupElement.querySelector(".button_close");
+let FormElement = popupElement.querySelector(".button_popup-save");
+
+function HandleProfileEditPopup() {
+  if (!popupElement.classList.contains("popup__opened")) {
+    let nameInput = popupElement.querySelector(".input_popup-name");
+    let jobInput = popupElement.querySelector(".input_popup-job");
+    nameInput.value = element.querySelector(".profile__name").textContent;
+    jobInput.value = element.querySelector(".profile__job").textContent;
+    popupElement.classList.toggle("popup__opened");
+  } else {
+    popupElement.classList.toggle("popup__opened");
+  }
+}
+
+editBtnElement.addEventListener("click", HandleProfileEditPopup);
+closeBtnElement.addEventListener("click", HandleProfileEditPopup);
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  let nameInput = popupElement.querySelector(".input_popup-name").value;
+  let jobInput = popupElement.querySelector(".input_popup-job").value;
+  element.querySelector(".profile__name").textContent = nameInput;
+  element.querySelector(".profile__job").textContent = jobInput;
+  HandleProfileEditPopup();
+}
+
+FormElement.addEventListener("click", handleProfileFormSubmit);

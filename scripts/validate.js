c79f3dcc7d35865be3f650d8b5aa086577880618
unknown <vinicius.barretto9022@gmail.com>
2024-11-28 10:37:23 -0300

    refactor: validate script

--- scripts/validate.js
+++ scripts/validate.js
@@ -17,12 +17,6 @@ const showInputError = (inputElement, errorMessage, errorElement, classObj) => {
   inputElement.classList.add(classObj.inputErrorClass);
 };
 
-/* Corrigido, foi um erro com o nome da classe da mensagem de erro, 
-estava com o nome do bloco do input, e antes de enviar o projeto mudei 
-para ficar com nome correto seguindo a metologia BEM e acabou mudando o 
-nome da classe do CSS que era para ser só adicionada quando o valor ficasse 
-invalido junto, resultando na mensagem ficando visivel */
-
 const hideInputError = (inputElement, errorElement, classObj) => {
   inputElement.classList.remove(classObj.inputErrorClass);
   errorElement.textContent = "";

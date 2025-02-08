93a10d6cf7c61e16519ee37875c9af31a238bd49
Vinimello90 <vinicius.barretto9022@gmail.com>
2025-02-08 15:26:48 -0300

    refactor: move Api class functions to index.js

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,191 +1,87 @@
-// Classe Api é responsável por manipular os dados
-// enviados e recebidos do perfil e dos cartões pela API
-class Api {
+// Classe Api é responsável por manipular os dados enviados e recebidos do perfil e dos cartões pela API
+export default class Api {
   constructor(options) {
     // Constructor recebe o options do fetch como parâmetro
-    this.baseUrl = options.baseUrl;
-    this.headers = options.headers;
+    this._baseUrl = options.baseUrl;
+    this._headers = options.headers;
   }
-  // Método público getInitialCards() é responsável por enviar a solicitação das informações
-  // dos cartões utilizando e retornando o método fetch com o método de solicitação "GET"
+
+  _checkResponse(res) {
+    return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
+  }
+
+  // Método público getInitialCards() é responsável por enviar a solicitação das informações dos cartões utilizando e retornando o método fetch com o método de solicitação "GET"
   getInitialCards() {
-    return fetch(`${this.baseUrl}/cards`, {
+    return fetch(`${this._baseUrl}/cards`, {
       headers: {
-        authorization: this.headers.authorization,
+        authorization: this._headers.authorization,
       },
     })
-      .then((res) =>
-        res.ok ? res.json() : Promise.reject(`Error: ${res.status}`)
-      )
-      .then((cards) => {
-        return cards;
-      })
+      .then(this._checkResponse)
       .catch((err) => console.log(err));
   }
-  // Método público addNewCard() é responsável por enviar a solicitação
-  // dos dados do novo car adicionado utilizando e retornando o método fetch com o método
-  // de solicitação "POST"
-  addNewCard({ body }) {
-    return fetch(`${this.baseUrl}/cards/`, {
+  // Método público addNewCard() é responsável por enviar a solicitação dos dados do novo cartão adicionado utilizando e retornando o método fetch com o método de solicitação "POST"
+  addNewCard(data) {
+    return fetch(`${this._baseUrl}/cards/`, {
       method: "POST",
-      headers: this.headers,
-      body: body,
+      headers: this._headers,
+      body: JSON.stringify(data),
     })
-      .then((res) =>
-        res.ok ? res.json() : Promise.reject(`Error: ${res.status}`)
-      )
-      .then((card) => card)
+      .then(this._checkResponse)
       .catch((err) => console.log(err));
   }
-  // Método público editLikeStatus() é responsável por enviar a solicitação
-  // para alterar o status do like utilizando e retornando o método fetch com os
-  // métodos de solicitações "PUT" para adicionar "true" e "DELETE" para remover
-  // mudando para "false".
+  // Método público editLikeStatus() é responsável por enviar a solicitação para alterar o status do like utilizando e retornando o método fetch com os métodos de solicitações "PUT"
+  // para adicionar "true" e "DELETE" para remover mudando para "false".
   editLikeStatus({ method, id }) {
-    return fetch(`${this.baseUrl}/cards/${id}/likes`, {
+    return fetch(`${this._baseUrl}/cards/${id}/likes`, {
       method: method,
       headers: {
-        authorization: this.headers.authorization,
+        authorization: this._headers.authorization,
       },
     })
-      .then((res) =>
-        res.ok ? res.json() : Promise.reject(`Error: ${res.status}`)
-      )
-      .then((userInfo) => userInfo)
+      .then(this._checkResponse)
       .catch((err) => console.log(err));
   }
-  // Método público deleteCard() é responsável por enviar a solicitação
-  // para remover o card através do id do cartão utilizando e retornando o método
-  // fetch com o método de solicitação "DELETE".
+  // Método público deleteCard() é responsável por enviar a solicitação para remover o card através do id do cartão utilizando e retornando o método fetch com o método de solicitação "DELETE".
   deleteCard(id) {
-    return fetch(`${this.baseUrl}/cards/${id}`, {
+    return fetch(`${this._baseUrl}/cards/${id}`, {
       method: "DELETE",
       headers: {
-        authorization: this.headers.authorization,
+        authorization: this._headers.authorization,
       },
     })
-      .then((res) =>
-        res.ok ? res.json() : Promise.reject(`Error: ${res.status}`)
-      )
-      .then((res) => res)
+      .then(this._checkResponse)
       .catch((err) => console.log(err));
   }
-  // Método público getUserInfo() é responsável por enviar a solicitação das informações
-  // do perfil do usuário utilizando e retornando o método fetch com o método
-  // de solicitação "GET"
+  // Método público getUserInfo() é responsável por enviar a solicitação das informações do perfil do usuário utilizando e retornando o método fetch com o método de solicitação "GET"
   getUserInfo() {
-    return fetch(`${this.baseUrl}/users/me`, {
+    return fetch(`${this._baseUrl}/users/me`, {
       headers: {
-        authorization: this.headers.authorization,
+        authorization: this._headers.authorization,
       },
     })
-      .then((res) =>
-        res.ok ? res.json() : Promise.reject(`Error: ${res.status}`)
-      )
-      .then((user) => {
-        return user;
-      })
+      .then(this._checkResponse)
       .catch((err) => console.log(err));
   }
-  // Método público editUserAvatar() é responsável  por enviar a solicitação da alteração
-  // do link da imagem do avatar do perfil do usuário utilizando e retornando o método fetch com o método
+  // Método público editUserAvatar() é responsável  por enviar a solicitação da alteração do link da imagem do avatar do perfil do usuário utilizando e retornando o método fetch com o método
   // de solicitação "PATCH"
-  editUserAvatar({ body }) {
-    return fetch(`${this.baseUrl}/users/me/avatar`, {
+  editUserAvatar(data) {
+    return fetch(`${this._baseUrl}/users/me/avatar`, {
       method: "PATCH",
-      headers: this.headers,
-      body: body,
+      headers: this._headers,
+      body: JSON.stringify(data),
     })
-      .then((res) =>
-        res.ok ? res.json() : Promise.reject(`Error: ${res.status}`)
-      )
-      .then((userInfo) => userInfo)
+      .then(this._checkResponse)
       .catch((err) => console.log(err));
   }
-  // Método público editUserInfo() é responsável  por enviar a solicitação da alteração
-  // das informações do perfil do usuário utilizando e retornando o método fetch com o método
-  // de solicitação "PATCH"
-  editUserInfo({ body }) {
-    return fetch(`${this.baseUrl}/users/me`, {
+  // Método público editUserInfo() é responsável  por enviar a solicitação da alteração das informações do perfil do usuário utilizando e retornando o método fetch com o método de solicitação "PATCH"
+  editUserInfo(data) {
+    return fetch(`${this._baseUrl}/users/me`, {
       method: "PATCH",
-      headers: this.headers,
-      body: body,
+      headers: this._headers,
+      body: JSON.stringify(data),
     })
-      .then((res) =>
-        res.ok ? res.json() : Promise.reject(`Error: ${res.status}`)
-      )
-      .then((userInfo) => userInfo)
+      .then(this._checkResponse)
       .catch((err) => console.log(err));
   }
 }
-
-// instância a classe Api para fazer as solicitações da API.
-const api = new Api({
-  baseUrl: "https://around-api.pt-br.tripleten-services.com/v1",
-  headers: {
-    authorization: "3104c43f-5c52-4781-879f-672ac8ed2b72",
-    "Content-Type": "application/json",
-  },
-});
-
-// Função fetchDataApi() retorna o método Promise.all() que tem como
-// parâmetro 2 métodos como callback da classe Api para solicitar
-// os dados do usuário e dos cartões
-function fetchDataApi() {
-  return Promise.all([api.getUserInfo(), api.getInitialCards()]);
-}
-
-// Função addNewCardApi() recebe um objeto com titulo e link do novo card como parâmetro
-// e retorna um método da classe Api que solicita para adicionar um novo cartão.
-function addNewCardApi({ title, link }) {
-  return api.addNewCard({
-    body: JSON.stringify({
-      name: title,
-      link: link,
-    }),
-  });
-}
-
-// Função editLikeApi() recebe um objeto com a id do cartão e o status do like como parâmetro
-// e retorna o método da Api que solicita para editar o status do like.
-function editLikeApi({ id, isLiked }) {
-  return api.editLikeStatus({
-    // verifica o status do like atual para definir o método de solicitação no parâmetro
-    method: !isLiked ? "PUT" : "DELETE",
-    id,
-  });
-}
-
-// Função editUserInfoApi() recebe um objeto com o nome e profissão como parâmetro
-// e retorna o método da Api que solicita para editar o perfil.
-function editUserInfoApi(name, job) {
-  return api.editUserInfo({
-    body: JSON.stringify({
-      name: name,
-      about: job,
-    }),
-  });
-}
-// Função editAvatarApi() recebe o link da imagem como parâmetro
-// e retorna o método da Api que solicita para editar o link do avatar.
-function editAvatarApi(avatar) {
-  return api.editUserAvatar({
-    body: JSON.stringify({
-      avatar: avatar,
-    }),
-  });
-}
-// Função deleteCardApi() recebe a id do cartão como parâmetro
-// e retorna o método da Api que solicita para editar o link do avatar.
-function deleteCardApi(id) {
-  return api.deleteCard(id);
-}
-
-export {
-  fetchDataApi,
-  addNewCardApi,
-  editLikeApi,
-  editUserInfoApi,
-  editAvatarApi,
-  deleteCardApi,
-};
--- src/pages/index.js
+++ src/pages/index.js
@@ -13,18 +13,19 @@ import Card from "../components/Card.js";
 import PopupWithImage from "../components/PopupWithImage.js";
 import PopupWithForm from "../components/PopupWithForm.js";
 import UserInfo from "../components/UserInfo.js";
-import {
-  fetchDataApi,
-  addNewCardApi,
-  editLikeApi,
-  editUserInfoApi,
-  deleteCardApi,
-  editAvatarApi,
-} from "../components/Api.js";
+import Api from "../components/Api.js";
 import PopupWithConfirmation from "../components/PopupWithConfirmation.js";
 
-//instância classe FormValidator responsável na
-//validação dos formulários.
+// instância a classe Api para fazer as solicitações da API.
+const api = new Api({
+  baseUrl: "https://around-api.pt-br.tripleten-services.com/v1",
+  headers: {
+    authorization: "3104c43f-5c52-4781-879f-672ac8ed2b72",
+    "Content-Type": "application/json",
+  },
+});
+
+//instância classe FormValidator responsável na validação dos formulários.
 const formValidator = new FormValidator(
   {
     formSelector: ".popup__form",
@@ -41,27 +42,23 @@ const formValidator = new FormValidator(
 //Inicializa a validação do formulário.
 formValidator.enableValidation();
 
-// Instacia a classe UserInfo para manipular
-// as informações do perfil do usuário.
+// Instacia a classe UserInfo para manipular as informações do perfil do usuário.
 const userInfo = new UserInfo({
   nameSelector: ".profile__name",
   jobSelector: ".profile__job",
   avatarSelector: ".profile__avatar",
 });
 
-// Ouvinte de eventos do botão de alterar o avatar para abrir a popup
-// do formulário de edição da imagem do avatar.
+// Ouvinte de eventos do botão de alterar o avatar para abrir a popup do formulário de edição da imagem do avatar.
 AvatarBtnElement.addEventListener("click", () => {
-  // instância a classe PopupWithForm() para abrir o popup do formulário
-  // para editar a imagem do avatar.
+  // instância a classe PopupWithForm() para abrir o popup do formulário para editar a imagem do avatar.
   const popupAvatar = new PopupWithForm({
     handleForm: ({ link }) => {
-      // Método loading("true") da classe Popup() que adiciona um aviso
-      // de salvando ao botão de submit e desativa o botão enquanto aguarda a solicitação da API.
+      // Método loading("true") da classe Popup() que adiciona um aviso de salvando ao botão de submit e desativa o botão enquanto aguarda a solicitação da API.
       popupAvatar.loading(true);
-      // Chama a função que chama o método da classe API, que solicita a alteração da imagem do avatar
-      // e retorna os dados alterados se tiver sucesso ao método then().
-      editAvatarApi(link)
+      // Chama o método da classe Api, que solicita a alteração da imagem do avatar e retorna os dados alterados se tiver sucesso ao método then().
+      api
+        .editUserAvatar({ avatar: link })
         .then((link) => {
           userInfo.setUserAvatar(link);
           popupAvatar.close();
@@ -75,23 +72,22 @@ AvatarBtnElement.addEventListener("click", () => {
   popupAvatar.open();
 });
 
-// Ouvinte de eventos de clique com com função anonima de callback,
-// que instância classe PopupwithForm para abrir o popup do formulário
-// para editar o perfil.
+// Ouvinte de eventos de clique com função anonima de callback, que instância a classe PopupwithForm para abrir o popup do formulário para editar o perfil.
 profileBtnElement.addEventListener("click", () => {
   const { currentName, currentJob } = userInfo.getUserInfo();
   document.forms.profile.name.value = currentName;
   document.forms.profile.job.value = currentJob;
-  // Chama o método resetInputValidation() da classe FormValidator
-  // para resetar e verificar novamente o formulário com as informações do perfil
-  // adicionadas, é passado como argumento o elemento do formulário.
+  // Chama o método resetInputValidation() da classe FormValidator com as informações do perfil adicionadas, é passado como argumento o elemento do formulário.
   formValidator.resetInputValidation(popupProfileForm);
   const popupProfile = new PopupWithForm({
-    handleForm: ({ name, job }) => {
+    handleForm: ({ name, job: about }) => {
       popupProfile.loading(true);
-      // Chama a função que chama o método da classe API, que solicita a alteração do perfil
-      // e retorna os dados alterados se tiver sucesso ao método then().
-      editUserInfoApi(name, job)
+      // Chama o método da classe Api, que solicita a alteração do perfil e retorna os dados alterados se tiver sucesso ao método then().
+      api
+        .editUserInfo({
+          name,
+          about,
+        })
         .then(({ name, about: job }) => {
           userInfo.setUserInfo({ name, job });
           popupProfile.close();
@@ -104,15 +100,12 @@ profileBtnElement.addEventListener("click", () => {
   popupProfile.open();
 });
 
-// Função que instância a classe Section que é responsável por iniciar a construção do card e
-// renderizar à página.
+// Função que instância a classe Section que é responsável por iniciar a construção dos cartões e renderizar à página.
 function generateCards(cards) {
   const addCard = new Section(
     {
       items: cards,
-      // renderer é um callback passado como argumento, recebe um objeto como
-      // parâmetro com os valores para o card, e instância classe Card para
-      // criação dos cards.
+      // renderer é um callback passado como argumento, recebe um objeto como parâmetro com os valores para o card, e instância classe Card para criação dos cards.
       renderer: (card) => {
         const createCard = new Card(
           {
@@ -122,22 +115,20 @@ function generateCards(cards) {
             userId: card.owner,
             ownerId: card.owner,
             cardId: card._id,
-            // handleCardClick é um callback que manipula os cliques dos ouvintes
-            //  de eventos do card, passado como argumento e recebe como parâmetro
-            // o evento e os valores do card para adicionar a popup.
+            // handleCardClick é um callback que manipula os cliques dos ouvintes de eventos do card, passado como argumento e recebe como parâmetro o evento
+            // e os valores do card para adicionar a popup.
             handleCardClick: (evt, { title, link, id, isLiked }) => {
               if (evt.target.classList.contains("button_remove")) {
-                // Instância classe PopupWithConfirmation() responsável pela remoção
-                // do cartã caso confirmado
+                // Instância classe PopupWithConfirmation() responsável pela remoção do cartão caso confirmado
                 const popupConfirmation = new PopupWithConfirmation({
                   handleConfirmation: () => {
-                    // Chama a função que chama o método da classe API, que solicita a remoção do cartão
-                    // e retorna ao método then() se tiver sucesso.
-                    deleteCardApi(id)
+                    // Chama o método da classe API, que solicita a remoção do cartão e retorna ao método then() se tiver sucesso.
+                    api
+                      .deleteCard(id)
                       .then(() => {
                         // remove o card da página.
                         evt.target.parentElement.remove();
-                        // verifica se contém card ainda na galeria.
+                        // verifica se contém cartões ainda na galeria.
                         createCard.handleRenderNoCards();
                       })
                       .finally(() => popupConfirmation.close());
@@ -148,16 +139,19 @@ function generateCards(cards) {
                 popupConfirmation.open();
               }
               if (evt.target.classList.contains("button_like")) {
-                // Chama a função que chama o método da classe API, que solicita a alteração do status do like
-                // e retorna ao método then() se tiver sucesso.
-                editLikeApi({ id, isLiked }).then(({ isLiked: status }) => {
-                  evt.target.classList.toggle("button_like_activate");
-                  createCard.setLikeStatus(status);
-                });
+                // Chama o método da classe API, que solicita a alteração do status do like e retorna ao método then() se tiver sucesso.
+                api
+                  .editLikeStatus({
+                    method: !isLiked ? "PUT" : "DELETE",
+                    id,
+                  })
+                  .then(({ isLiked: status }) => {
+                    evt.target.classList.toggle("button_like_activate");
+                    createCard.setLikeStatus(status);
+                  });
               }
               if (evt.target.classList.contains("card__image")) {
-                // instância classe PopupWithImage passando como argumento
-                // os dados do card.
+                // instância classe PopupWithImage passando como argumento os dados do card.
                 const popupWithImage = new PopupWithImage(
                   {
                     title: title,
@@ -172,12 +166,9 @@ function generateCards(cards) {
           },
           galleryCardsElement
         );
-        // método da classe Section que é pasasdo como callback para criar o
-        // elemento e receber como retorno o elemento para renderizar o card na página.
+        // método da classe Section que é passado como callback para criar o elemento e receber como retorno o elemento para renderizar o card na página.
         addCard.addItem(createCard.renderCard());
-        // confere se no elemento na variavel noCards está ausente a classe que esconde ele,
-        // caso verdadeiro chama a o método da classe Card que confere se há cards ou não para
-        // para renderizar a mensagem caso não haja cards.
+        // confere ausente a classe no elemento, caso verdadeiro chama a o método da classe Card que verifica se contém cartões ainda na galeria para renderizar a mensagem caso não haja.
         !noCards.classList.contains("no-cards_hidden")
           ? createCard.handleRenderNoCards()
           : null;
@@ -188,21 +179,20 @@ function generateCards(cards) {
   addCard.renderer();
 }
 
-// Ouvinte de eventos do botão que abre a popup do formulário
-// para adicionar um novo cartão.
+// Ouvinte de eventos do botão que abre a popup do formulário para adicionar um novo cartão.
 addBtnElement.addEventListener("click", () => {
   const popupAddCard = new PopupWithForm({
-    // handleForm é um callback passado como argumento, que recebe os valores como
-    // parâmetro e instância classe Section que fica responsável por
-    // renderizar na página o card.
-    handleForm: ({ title, link }) => {
+    // handleForm é um callback passado como argumento, que instância classe Section responsável por renderizar na página o card.
+    handleForm: ({ title: name, link }) => {
       popupAddCard.loading(true);
-      // Chama a função que chama o método da classe API, que solicita adição dos dados
-      // do novo cartão e retorna ao método then() se tiver sucesso.
-      addNewCardApi({ title, link })
+      // solicita adição dos dados do novo cartão à API e retorna ao método then() se tiver sucesso.
+      api
+        .addNewCard({
+          name,
+          link,
+        })
         .then((card) => {
-          // função  instância a classe Section que é responsável por iniciar a construção do card e
-          // renderizar à página.
+          // função  instância a classe Section que é responsável por iniciar a construção do card e renderizar à página.
           generateCards([card]);
           popupAddCard.close();
         })
@@ -214,9 +204,8 @@ addBtnElement.addEventListener("click", () => {
   popupAddCard.open();
 });
 
-// Função fetchDataApi() chama o método da classe Api, que solicita os dados
-// do usuário e dos cartões e retorna ao método then().
-fetchDataApi().then(([user, cards]) => {
+// solicita os dados do perfil da API.
+api.getUserInfo().then((user) => {
   // Renderiza as informações do perfil à página.
   userInfo.setUserInfo({
     name: user.name,
@@ -224,10 +213,11 @@ fetchDataApi().then(([user, cards]) => {
   });
   // Renderiza o avatar do perfil à página.
   userInfo.setUserAvatar({ avatar: user.avatar });
-  // função  instância a classe Section que é responsável por iniciar a construção do card e
-  // renderizar à página, é passado como argumento a array com os dados dos cartões usando
-  // o método reverse() para inveter a ordem da array e carregar os dados na ordem
-  // correta.
+});
+
+// solicita os dados dos cartões iniciais da API.
+api.getInitialCards().then((cards) => {
+  // função generateCards() instância a classe Section que é responsável por iniciar a construção do card e renderizar à página.
   generateCards(cards.reverse());
 });
 
